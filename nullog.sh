4871ed6d22ec5866115d080593716268e2ad6b7d
MrEmpy <71796063+MrEmpy@users.noreply.github.com>
2021-02-05 20:58:41 -0300

    Update nullog.sh

--- nullog.sh
+++ nullog.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/sh
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
 printf '\033[0;31m
      ████               ████
     ███                   ███
