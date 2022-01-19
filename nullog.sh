04024601b2d7923537ffbe3c2d1cce8d140266ef
MrEmpy <71796063+MrEmpy@users.noreply.github.com>
2022-01-18 22:30:55 -0300

    Update nullog.sh

--- nullog.sh
+++ nullog.sh
@@ -1,76 +1,84 @@
 #!/usr/bin/sh
-printf '\033[0;31m
-     ████               ████
-    ███                   ███
-   ███                     ███
-  ███                       ███
- ███                         ███
-████                         ████
-████                         ████
-██████       ███████       ██████
-█████████████████████████████████
-█████████████████████████████████
- ███████████████████████████████ 
-  ████ ███████████████████ ████  
-       ███▀███████████▀███       
-      ████──▀███████▀──████      
-      █████───█████───█████      
-      ███████▄█████▄███████      
-       ███████████████████       
-        █████████████████        
-         ███████████████         
-          █████████████          
-           ███████████           
-          ███──▀▀▀──███          
-          ███─█████─███          
-           ███─███─███           
-            █████████
 
-\033[0;31m  _   _       _\033[0;37m _             
-\033[0;31m | \ | |_   _| \033[0;37m| | ___   __ _ 
-\033[0;31m |  \| | | | | \033[0;37m| |/ _ \ / _` |
-\033[0;31m | |\  | |_| | \033[0;37m| | (_) | (_| |
-\033[0;31m |_| \_|\__,_|_\033[0;37m|_|\___/ \__, |
-\033[0;31m               \033[0;37m         |___/ 
+banner() {
+    printf '\033[0;31m
+         ████               ████
+        ███                   ███
+       ███                     ███
+      ███                       ███
+     ███                         ███
+    ████                         ████
+    ████                         ████
+    ██████       ███████       ██████
+    █████████████████████████████████
+    █████████████████████████████████
+    ███████████████████████████████ 
+      ████ ███████████████████ ████  
+           ███▀███████████▀███       
+          ████──▀███████▀──████      
+          █████───█████───█████      
+          ███████▄█████▄███████      
+           ███████████████████       
+            █████████████████        
+             ███████████████         
+              █████████████          
+               ███████████           
+              ███──▀▀▀──███          
+              ███─█████─███          
+               ███─███─███           
+                █████████
+    \033[0;31m  _   _       _\033[0;37m _             
+    \033[0;31m | \ | |_   _| \033[0;37m| | ___   __ _ 
+    \033[0;31m |  \| | | | | \033[0;37m| |/ _ \ / _` |
+    \033[0;31m | |\  | |_| | \033[0;37m| | (_) | (_| |
+    \033[0;31m |_| \_|\__,_|_\033[0;37m|_|\___/ \__, |
+    \033[0;31m               \033[0;37m         |___/ 
+
+            \033[0;31m[\033[0;37mVersion 2.0\033[0;31m]
+
 '
-sleep 1
+}
 
-######### Excluindo logs de login ##########
-printf '\n\033[0;34m[*] \033[0;37mLimpando logs de login no sistema\n'
-sleep 1
-echo >/var/log/wtmp
-echo >/var/log/btmp
-echo >/var/log/lastlog
-printf '\033[0;32m[+] \033[0;37mLogs de login apagado com sucesso\n'
-sleep 1
+remove_login() {
+    echo > /var/log/wtmp
+    echo > /var/log/btmp
+    echo > /var/log/lastlog
+}
 
-######### Limpar .bashrc  ##########
+bash_history() {
+    for bash_history in $(find / -name ".bash_history" 2>/dev/null)
+        do
+            echo > $bash_history
+    done
+}
 
-printf '\n\033[0;34m[*] \033[0;37mLimpando historicos no sistema\n'
-sleep 1
-find / -name ".bash_history" 2>/dev/null > .history
-while read history; do
-    echo > $history 2>&1
-    printf "\n\033[0;32m[+] \033[0;37mHistorico apagado: $history"
-done<.history
-rm -rf .history
-printf '\n\n\033[0;32m[+] \033[0;37mHistoricos de login apagado com sucesso\n'
-sleep 1
+logs_f() {
+    for log_f in $(find / -name "*\.log\.*" 2>/dev/null;find / -name "*\.log\.*\.*" 2>/dev/null;find / -name "*\.*\.log\.*" 2>/dev/null)
+        do
+            echo > $log_f
+    done
+}
 
-######### Excluindo conteudo de .log ##########
-printf '\n\033[0;34m[*] \033[0;37mProcurando por arquivos log no sistema\n'
-find / -name "*.log" 2>/dev/null > .logs
-countLogs='wc -l <.logs'
+main() {
+    banner
+    sleep 1
+    printf '\n\033[0;34m[*] \033[0;37mClearing system login logs\n'
+    sleep 1
+    remove_login
+    printf '\033[0;32m[+] \033[0;37mLogin logs successfully deleted!\n'
+    sleep 1
+    printf '\n\033[0;34m[*] \033[0;37mClearing system histories\n'
+    bash_history
+    printf '\n\n\033[0;32m[+] \033[0;37mSystem history deleted successfully!\n'
+    sleep 1
+    printf '\n\033[0;34m[*] \033[0;37mDeleting content inside log files'
+    logs_f
+    printf '\n\n\033[0;32m[+] \033[0;37mLog files cleared successfully!\n'
+    printf '\n\033[0;34m[*] \033[0;37mClear command history "history -c"\n'
+}
 
-printf '\033[0;32m[+] \033[0;37mArquivos log encontrado'
-sleep 1
-printf '\n\033[0;34m[*] \033[0;37mApagando conteudo dentro dos arquivos log'
-
-while read log; do
-    echo > $log 2>&1
-    printf "\n\033[0;32m[+] \033[0;37mLogs apagado: $log"
-done<.logs
-rm -rf .logs
-
-printf '\n\n\033[0;32m[+] \033[0;37mArquivos log limpo com sucesso\n'
-printf '\n\033[0;34m[*] \033[0;37mLimpe o historico de comandos "history -c"\n'
+if [[ $EUID -ne 0 ]]; then
+   printf "\033[0;34m[*] \033[0;37mYou are not running Nullog as root, for that reason only some logs that you have permission will be deleted. Do you really want to continue? [Press ENTER]" 
+   read
+   main
+fi
